a8ee4dd44e87dd74bc919f1c8f83b039f33c2072
aylenbartomeo <abartomeo@fi.uba.ar>
2026-05-08 12:47:47 -0300

    Agrego interfaz base para items

---
+++ server/src/model/items/Item.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_SRC_MODEL_ITEMS_ITEM_H
+#define SERVER_SRC_MODEL_ITEMS_ITEM_H
+
+#include <string>
+
+class Item {
+public:
+    virtual ~Item() = default;
+
+    virtual int getId() const = 0;
+    virtual const std::string& getName() const = 0;
+};
+
+#endif
