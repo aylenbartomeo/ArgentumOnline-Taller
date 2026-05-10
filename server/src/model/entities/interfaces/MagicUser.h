41f2a6ea285351061fb4781aa471415c6056a3ce
Alexander Coronado <acoronado@fi.uba.ar>
2026-05-10 12:31:38 -0300

    Interfaz para metodos de mana

---
+++ server/src/model/entities/interfaces/MagicUser.h
@@ -0,0 +1,15 @@
+#ifndef MAGIC_USER_H
+#define MAGIC_USER_H
+
+#include <cstdint>
+
+class MagicUser {
+public:
+    virtual ~MagicUser() = default;
+
+    virtual uint16_t get_intelligence() const = 0;
+    virtual int get_mana() const = 0;
+    virtual void consume_mana(int amount) = 0;
+};
+
+#endif
