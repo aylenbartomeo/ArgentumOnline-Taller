87bd5e71d71ececf031dfa66252ab58ea6b9fee9
Bruno Pezman <brunopezman@gmail.com>
2026-05-15 14:51:00 -0300

    Unificacion de enums de comandos en archivo CommandDTO.h

---
+++ common/src/CommandDTO.h
@@ -0,0 +1,106 @@
+#ifndef COMMAND_DTO_H
+#define COMMAND_DTO_H
+
+#define MEDITATE_CMD "/meditar"
+#define RESPAWN_CMD "/resucitar"
+#define HEAL_CMD "/curar"
+#define BUY_CMD "/comprar"
+#define SELL_CMD "/vender"
+#define DEPOSIT_CMD "/depositar"
+#define WITHDRAW_CMD "/retirar"
+#define TAKE_CMD "/tomar"
+#define DROP_CMD "/tirar"
+
+#define FOUND_CLAN_CMD "/fundar-clan"
+#define JOIN_CLAN_CMD "/unirse-clan"
+#define LEAVE_CLAN_CMD "/dejar-clan"
+#define CHECK_CLAN_CMD "/revisar-clan"
+#define ACCEPT_CLAN_CMD "/aceptar-clan"
+#define REJECT_CLAN_CMD "/rechazar-clan"
+#define KICK_CLAN_CMD "/clan-kick"
+
+enum class PlayerState {
+    ALIVE,
+    GHOST,
+    MEDITATING,
+};
+
+struct ItemInfoDTO {
+    uint32_t itemId;
+    uint16_t quantity;
+    bool isEquipped;
+};
+
+struct InventoryStateDTO {
+    std::vector<ItemInfoDTO> items;
+    uint32_t safeGold;
+    uint32_t excessGold;
+
+    uint32_t equippedWeaponId;
+    uint32_t equippedArmorId;
+    uint32_t equippedHelmetId;
+    uint32_t equippedShieldId;
+};
+
+enum class ActionItemType : uint8_t { 
+    TAKE, 
+    DROP, 
+    EQUIP, 
+    UNEQUIP 
+};
+
+struct ItemCommandDTO {
+    ActionItemType action;
+    uint8_t inventorySlot;
+};
+
+enum class Movement : uint8_t { 
+    UP, 
+    DOWN, 
+    LEFT, 
+    RIGHT, 
+    DIAGONAL_UP_LEFT,
+    DIAGONAL_UP_RIGHT,
+    DIAGONAL_DOWN_LEFT,
+    DIAGONAL_DOWN_RIGHT,
+    STOP 
+};
+
+enum class interactCitizen : uint8_t {
+    RESPAWN,
+    HEAL,
+    BUY,
+    SELL,
+    DEPOSIT,
+    WITHDRAW
+};
+
+enum class ClanCommandType : uint8_t {
+    FOUND,
+    JOIN,
+    LEAVE,
+    CHECK,
+    ACCEPT,
+    REJECT,
+    KICK
+};
+
+enum class ActionType : uint8_t {
+    MOVE,
+    ATTACK,
+    INTERACT,
+    USE_ITEM,
+    CLAN_ACTION
+};
+
+struct CommandDTO {
+    enum ActionType type;
+    enum Movement movement;
+    enum interactCitizen interactAction;
+    enum ClanCommandType clanAction;
+    uint8_t id_jugador;
+    float angle;
+
+};
+
+#endif
