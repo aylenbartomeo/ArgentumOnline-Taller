0b49bc201cab620aac65eb1189b24e151240ab79
aylenbartomeo <abartomeo@fi.uba.ar>
2026-06-11 10:52:42 -0300

    primer intento de tile de agua y pisos de ciudad

--- common/src/TerrainRegistry.cpp
+++ common/src/TerrainRegistry.cpp
@@ -3,7 +3,7 @@
 const std::vector<TerrainDef>& getTerrainRegistry() {
     static const std::vector<TerrainDef> registry = {
             {"5108.png", 416, 384, "pasto"},
-            {"5108.png", 416, 384, "agua"},
+            {"5108.png", 448, 128, "agua"},
             {"5108.png", 416, 384, "camino"},
             {"5108.png", 512, 480, "empedrado"},
             {"5108.png", 512, 480, "pisoMadera"},
