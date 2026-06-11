5de0006bf6bc93916fb0ee4fb980ebf7e3d56966
aylenbartomeo <abartomeo@fi.uba.ar>
2026-06-11 10:49:58 -0300

    uso pasto oscuro para los pisos de ciudad (distinto del campo)

---
+++ common/src/TerrainRegistry.cpp
@@ -0,0 +1,13 @@
+#include "TerrainRegistry.h"
+
+const std::vector<TerrainDef>& getTerrainRegistry() {
+    static const std::vector<TerrainDef> registry = {
+            {"5108.png", 416, 384, "pasto"},
+            {"5108.png", 416, 384, "agua"},
+            {"5108.png", 416, 384, "camino"},
+            {"5108.png", 512, 480, "empedrado"},
+            {"5108.png", 512, 480, "pisoMadera"},
+            {"5108.png", 512, 480, "pisoPiedra"},
+    };
+    return registry;
+}
