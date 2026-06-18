f896b61a57da98cef27c49df3c515af5748fba7d
aylenbartomeo <abartomeo@fi.uba.ar>
2026-06-18 16:12:43 -0300

    agrando el tamaño fijo de los mapas nuevos a 100x100

--- editor/MapDefaults.h
+++ editor/MapDefaults.h
@@ -2,8 +2,8 @@
 #define MAP_DEFAULTS_H
 
 namespace MapDefaults {
-constexpr int WIDTH = 60;
-constexpr int HEIGHT = 60;
+constexpr int WIDTH = 100;
+constexpr int HEIGHT = 100;
 constexpr int TILE_SIZE = 32;
 constexpr const char* TILESET = "5108.png";
 constexpr int TILESET_COLS = 32;
